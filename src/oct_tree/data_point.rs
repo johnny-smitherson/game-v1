f5cf7d4fa21fe59260ffff4abcc2c1c58a457046
johnny-smitherson <127537716+johnny-smitherson@users.noreply.github.com>
2023-11-10 03:28:22 +0000

    cargo fmt

--- src/oct_tree/data_point.rs
+++ src/oct_tree/data_point.rs
@@ -4,12 +4,12 @@ use bevy::math::Vec3;
 
 /// a point in 3d space with a unique id
 pub struct DataPoint {
-  pub id: u32,
-  pub position: Vec3,
+    pub id: u32,
+    pub position: Vec3,
 }
 
 impl DataPoint {
-  pub fn new(id: u32, position: Vec3) -> Self {
-    Self { id, position }
-  }
+    pub fn new(id: u32, position: Vec3) -> Self {
+        Self { id, position }
+    }
 }
